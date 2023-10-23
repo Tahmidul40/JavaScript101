b0f64f15dcaeda3e4237ea26c3591a619417fb23
Tahmidul40 <77900139+Tahmidul40@users.noreply.github.com>
2023-10-23 17:34:38 +0000

    Strings, number and math

---
+++ 07_basicStrings.js
@@ -0,0 +1,38 @@
+const name = "Tahmidul";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Tahmid");
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("t"));
+// console.log(gameName.trim());
+// console.log(gameName.bold()); 
+
+const newStringTwo = "Tahmid-ul"
+const newString = newStringTwo
+
+// console.log(newStringTwo);
+// console.log(newString);
+
+// console.log(newString.substring(0, 6));
+// console.log(newString.slice(0, 4));
+// console.log(newString.split('-'));
+
+const gamerName = "   flash   "
+// console.log(gamerName)
+// console.log(gamerName.trim())
+
+
+const url = "http://example.com/example20%website"
+// console.log(url.replace('20%', '-'));
+// console.log(url.includes('example'));
+
+
+
---
+++ 08_basicNumberMath.js
@@ -0,0 +1,41 @@
+const score = 400
+const balance = new Number(100)
+
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed());
+
+const otherNumber = 23.343233423
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000000
+
+// console.log(hundreds.toLocaleString());
+
+
+
+/*---------------------------------------*/
+/*              Math                     */
+/*---------------------------------------*/
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(10.25));
+// console.log(Math.ceil(10.25));
+// console.log(Math.floor(10.25));
+// console.log(Math.max(4, 30, 42, 343, 23, 34));
+// console.log(Math.min(4, 30, 42, 343, 23, 34));
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
